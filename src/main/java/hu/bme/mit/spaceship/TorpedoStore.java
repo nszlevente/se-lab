f15f0abd482455455f9ad714016806c483cb4b4e
unknown <184403@BME.HU>
2024-10-09 17:08:10 +0200

    comment

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -8,7 +8,7 @@ import java.util.Random;
 * (Deliberately contains bugs.)
 */
 public class TorpedoStore {
-
+  //Helóka
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
   private Random generator = new Random();
